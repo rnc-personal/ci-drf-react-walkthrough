96c881f5b731a4c8264e83d3f79edc90f25631a3
rnc-personal <richard.challenger@protonmail.com>
2023-07-03 18:31:07 +0000

    adding check for duplicate likes

---
+++ likes/serializers.py
@@ -0,0 +1,22 @@
+form django.db import IntegrityError
+from rest_framework import serializers
+from .models import Like
+
+class ProfileSerializer(serializers.ModelSerializer):
+
+    owner = serializers.ReadOnlyField(source='owner.username')
+    
+    class Meta:
+        model = Like
+        fields = [
+            'id',
+            'owner',
+            'post',
+            'created_at',
+        ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError('User already liked this post')
